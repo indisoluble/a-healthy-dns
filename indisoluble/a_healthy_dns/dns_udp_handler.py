5b281c83b1a9fa54d98e64f5f2a5b7dab7d9b937
Enrique de la Torre <indisoluble_dev@me.com>
2025-04-05 18:49:57 +0200

    Refactor dns_udp_handler

--- indisoluble/a_healthy_dns/dns_udp_handler.py
+++ indisoluble/a_healthy_dns/dns_udp_handler.py
@@ -10,71 +10,79 @@ import dns.rrset
 from .dns_server_config import DNSServerConfig
 
 
+def _handle_a_record(
+    response: dns.message.Message, qname: str, config: DNSServerConfig
+):
+    if qname not in config.resolutions:
+        logging.warning("Received query for unknown domain: %s", qname)
+        response.set_rcode(dns.rcode.NXDOMAIN)
+        return
+
+    ips = config.resolutions[qname]
+
+    rrset = dns.rrset.from_text(
+        qname, config.ttl_a, dns.rdataclass.IN, dns.rdatatype.A, *ips
+    )
+    response.answer.append(rrset)
+    logging.debug("Responded to A query for %s with %s", qname, ips)
+
+
+def _handle_ns_record(
+    response: dns.message.Message, qname: str, config: DNSServerConfig
+):
+    if qname != config.hosted_zone:
+        logging.warning("Received NS query for unknown zone: %s", qname)
+        response.set_rcode(dns.rcode.NXDOMAIN)
+        return
+
+    rrset = dns.rrset.from_text(
+        qname,
+        config.ttl_ns,
+        dns.rdataclass.IN,
+        dns.rdatatype.NS,
+        *config.name_servers,
+    )
+    response.answer.append(rrset)
+    logging.debug("Responded to NS query for %s with %s", qname, config.name_servers)
+
+
+def _handle_soa_record(
+    response: dns.message.Message, qname: str, config: DNSServerConfig
+):
+    if qname != config.hosted_zone:
+        logging.warning("Received SOA query for unknown zone: %s", qname)
+        response.set_rcode(dns.rcode.NXDOMAIN)
+        return
+
+    rrset = dns.rrset.from_text(
+        qname,
+        config.ttl_a,
+        dns.rdataclass.IN,
+        dns.rdatatype.SOA,
+        " ".join(
+            [
+                config.primary_name_server,
+                f"hostmaster.{config.hosted_zone}",
+                str(config.soa_serial),
+                str(config.soa_refresh),
+                str(config.soa_retry),
+                str(config.soa_expire),
+                str(config.ttl_a),
+            ]
+        ),
+    )
+    response.answer.append(rrset)
+    logging.debug("Responded to SOA query for %s", qname)
+
+
+_HANDLERS = {
+    dns.rdatatype.A: _handle_a_record,
+    dns.rdatatype.NS: _handle_ns_record,
+    dns.rdatatype.SOA: _handle_soa_record,
+}
+
+
 class DNSUDPHandler(socketserver.BaseRequestHandler):
-    def _response_for_a_record(self, response, qname):
-        config: DNSServerConfig = self.server.config
-
-        if qname not in config.resolutions:
-            logging.warning("Received query for unknown domain: %s", qname)
-            response.set_rcode(dns.rcode.NXDOMAIN)
-            return
-
-        ips = config.resolutions[qname]
-
-        rrset = dns.rrset.from_text(
-            qname, config.ttl_a, dns.rdataclass.IN, dns.rdatatype.A, *ips
-        )
-        response.answer.append(rrset)
-        logging.debug("Responded to A query for %s with %s", qname, ips)
-
-    def _response_for_ns_record(self, response, qname):
-        config: DNSServerConfig = self.server.config
-
-        if qname != config.hosted_zone:
-            logging.warning("Received NS query for unknown zone: %s", qname)
-            response.set_rcode(dns.rcode.NXDOMAIN)
-            return
-
-        rrset = dns.rrset.from_text(
-            qname,
-            config.ttl_ns,
-            dns.rdataclass.IN,
-            dns.rdatatype.NS,
-            *config.name_servers,
-        )
-        response.answer.append(rrset)
-        logging.debug(
-            "Responded to NS query for %s with %s", qname, config.name_servers
-        )
-
-    def _response_for_soa_record(self, response, qname):
-        config: DNSServerConfig = self.server.config
-
-        if qname != config.hosted_zone:
-            logging.warning("Received SOA query for unknown zone: %s", qname)
-            response.set_rcode(dns.rcode.NXDOMAIN)
-            return
-
-        rrset = dns.rrset.from_text(
-            qname,
-            config.ttl_a,
-            dns.rdataclass.IN,
-            dns.rdatatype.SOA,
-            " ".join(
-                [
-                    config.primary_name_server,
-                    f"hostmaster.{config.hosted_zone}",
-                    str(config.soa_serial),
-                    str(config.soa_refresh),
-                    str(config.soa_retry),
-                    str(config.soa_expire),
-                    str(config.ttl_a),
-                ]
-            ),
-        )
-        response.answer.append(rrset)
-        logging.debug("Responded to SOA query for %s", qname)
-
     def handle(self):
         data, sock = self.request
 
@@ -93,12 +101,10 @@ class DNSUDPHandler(socketserver.BaseRequestHandler):
         else:
             question = query.question[0]
 
-            if question.rdtype == dns.rdatatype.A:
-                self._response_for_a_record(response, question.name.to_text())
-            elif question.rdtype == dns.rdatatype.NS:
-                self._response_for_ns_record(response, question.name.to_text())
-            elif question.rdtype == dns.rdatatype.SOA:
-                self._response_for_soa_record(response, question.name.to_text())
+            if question.rdtype in _HANDLERS:
+                _HANDLERS[question.rdtype](
+                    response, question.name.to_text(), self.server.config
+                )
             else:
                 logging.warning("Received unsupported query type: %s", question.rdtype)
                 response.set_rcode(dns.rcode.NOTIMP)
