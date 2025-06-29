c82358fe4fe7066daed38263da5608cdbf5695d2
Enrique de la Torre <indisoluble_dev@me.com>
2025-06-29 20:58:54 +0200

    Put normalize_ip.py under test

--- tests/indisoluble/a_healthy_dns/tools/test_normalize_ip.py
+++ tests/indisoluble/a_healthy_dns/tools/test_normalize_ip.py
@@ -5,40 +5,43 @@ import pytest
 from indisoluble.a_healthy_dns.tools.normalize_ip import normalize_ip
 
 
-def test_normalize_ip_no_leading_zeros():
-    ip_addresses = [
+@pytest.mark.parametrize(
+    "ip",
+    [
         "192.168.1.1",
         "10.0.0.1",
         "172.16.0.1",
         "127.0.0.1",
         "0.0.0.0",
         "255.255.255.255",
-    ]
+    ],
+)
+def test_normalize_ip_no_leading_zeros(ip):
+    assert normalize_ip(ip) == ip
 
-    for ip in ip_addresses:
-        assert normalize_ip(ip) == ip
 
-
-def test_normalize_ip_with_leading_zeros():
-    test_cases = [
+@pytest.mark.parametrize(
+    "input_ip,expected_output",
+    [
         ("192.168.001.001", "192.168.1.1"),
         ("010.020.030.040", "10.20.30.40"),
         ("001.002.003.004", "1.2.3.4"),
         ("192.0168.1.1", "192.168.1.1"),
-    ]
+    ],
+)
+def test_normalize_ip_with_leading_zeros(input_ip, expected_output):
+    assert normalize_ip(input_ip) == expected_output
 
-    for input_ip, expected_output in test_cases:
-        assert normalize_ip(input_ip) == expected_output
 
-
-def test_normalize_ip_with_all_zeros():
-    test_cases = [
+@pytest.mark.parametrize(
+    "input_ip,expected_output",
+    [
         ("000.168.1.1", "0.168.1.1"),
         ("192.000.1.1", "192.0.1.1"),
         ("192.168.000.1", "192.168.0.1"),
         ("192.168.1.000", "192.168.1.0"),
         ("000.000.000.000", "0.0.0.0"),
-    ]
-
-    for input_ip, expected_output in test_cases:
-        assert normalize_ip(input_ip) == expected_output
+    ],
+)
+def test_normalize_ip_with_all_zeros(input_ip, expected_output):
+    assert normalize_ip(input_ip) == expected_output
