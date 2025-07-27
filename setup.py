b70d8b59353b2f4942891d7ed658b61d6d89877d
Enrique de la Torre <indisoluble_dev@me.com>
2025-07-27 21:54:24 +0200

    Dockerfile: use latest python-alpine image

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="a_healthy_dns",
-    version="0.1.7",
+    version="0.1.8",
     description="A healthy DNS project",
     packages=find_packages(),
     python_requires=">=3.9",
