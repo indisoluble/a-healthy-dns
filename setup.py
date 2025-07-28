0cdf205779998c0c3db0f0c1dee7aaaa2b2996cc
Enrique de la Torre <indisoluble_dev@me.com>
2025-07-28 21:41:35 +0200

    Publish docker to Docker Hub

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="a_healthy_dns",
-    version="0.1.8",
+    version="0.1.9",
     description="A healthy DNS project",
     packages=find_packages(),
     python_requires=">=3.9",
